e9c41c706f3d99eb89c79204d9dccacf8e9163d8
Anna Smolskaya <anna31rus@mail.ru>
2023-05-25 21:46:38 +0500

    fixed CORS connection

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const { PORT = 3000 } = process.env;
 // создаем инстанс сервера
 const app = express();
 // добавить cors как мидлвар
-app.use = (cors());
+app.use(cors());
 
 app.use(express.json());
 app.use(helmet());
